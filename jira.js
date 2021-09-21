d566d2f0a5d5c649dc0941f9172265bbabc51adc
Snicked <alex.funk.1988@googlemail.com>
2021-09-21 11:07:32 +0200

    Update jira.js

--- jira.js
+++ jira.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://octagen.atlassian.net/*
+// @match        https://myJira.atlassian.net/*
 // @grant        none
 // ==/UserScript==
 
